cac9cf51e2f3cff0c1217a77e43667d5e94dd24a
Montel Laurent <montel@kde.org>
2017-04-25 08:12:32 +0200

    Exclude autotests too

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/' | grep -v '/kwatchgnupg/'` -o $podir/kleopatra.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/' | grep -v '/autotests/' | grep -v '/kwatchgnupg/'` -o $podir/kleopatra.pot
 rm -f rc.cpp

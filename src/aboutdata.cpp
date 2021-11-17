2eda5760d40028d94b9ce4cf2f8aa757c8083750
Ingo Klöcker <dev@ingo-kloecker.de>
2021-11-17 10:49:51 +0100

    Include KF5 headers as camel-case headers
    and don't look for ki18n_version.h next to our source files.
    
    CCMAIL: montel@kde.org

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -15,9 +15,9 @@
 #include <Libkleo/GnuPG>
 
 #include <KLocalizedString>
-#include "ki18n_version.h"
+#include <ki18n_version.h>
 #if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
-#include <klazylocalizedstring.h>
+#include <KLazyLocalizedString>
 #undef I18N_NOOP
 #define I18N_NOOP kli18n
 #endif

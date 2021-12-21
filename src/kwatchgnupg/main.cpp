439028ce08cde62b9301743c67da0f87964e4fc5
Laurent Montel <montel@kde.org>
2021-12-21 18:47:53 +0100

    GIT_SILENT: AA_EnableHighDpiScaling is by default in qt6 now

--- src/kwatchgnupg/main.cpp
+++ src/kwatchgnupg/main.cpp
@@ -26,7 +26,9 @@
 int main(int argc, char **argv)
 {
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QApplication app(argc, argv);
     KCrash::initialize();
 #if KCOREADDONS_VERSION < QT_VERSION_CHECK(6, 0, 0)
--- src/main.cpp
+++ src/main.cpp
@@ -99,7 +99,9 @@ static void fillKeyCache(Kleo::UiServer *server)
 
 int main(int argc, char **argv)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
     KleopatraApplication app(argc, argv);
     KCrash::initialize();

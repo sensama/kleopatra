e226b92083999e25ab684a39e324740732ff59c8
Laurent Montel <montel@kde.org>
2021-12-23 08:11:31 +0100

    GIT_SILENT: it's enabled by default too in qt6

--- src/kwatchgnupg/main.cpp
+++ src/kwatchgnupg/main.cpp
@@ -25,9 +25,9 @@
 
 int main(int argc, char **argv)
 {
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 #endif
     QApplication app(argc, argv);
     KCrash::initialize();
--- src/main.cpp
+++ src/main.cpp
@@ -101,8 +101,8 @@ int main(int argc, char **argv)
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+#endif
     KleopatraApplication app(argc, argv);
     KCrash::initialize();
     QAccessible::installFactory(Kleo::accessibleWidgetFactory);

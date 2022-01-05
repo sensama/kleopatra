24df800db3151e7b309116bf3d0bd01df009e65d
Ingo Klöcker <dev@ingo-kloecker.de>
2022-01-05 10:48:27 +0100

    Fix filename in header comment

--- src/commands/exportpaperkeycommand.h
+++ src/commands/exportpaperkeycommand.h
@@ -1,5 +1,5 @@
 /* -*- mode: c++; c-basic-offset:4 -*-
-    commands/exportsecretkeycommand.h
+    commands/exportpaperkeycommand.h
 
     This file is part of Kleopatra, the KDE keymanager
     SPDX-FileCopyrightText: 2016 Bundesamt für Sicherheit in der Informationstechnik

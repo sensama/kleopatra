d780a8111f48926ad3073b9516174b7bb081f508
Ingo Klöcker <dev@ingo-kloecker.de>
2022-05-16 12:37:15 +0200

    Move ChooseProtocolPage to file of its own
    GnuPG-bug-id: 5969

--- src/newcertificatewizard/newcertificatewizard.cpp
+++ src/newcertificatewizard/newcertificatewizard.cpp
@@ -16,6 +16,7 @@
 
 #include <settings.h>
 
+#include "chooseprotocolpage_p.h"
 #include "ui_enterdetailspage.h"
 #include "ui_keycreationpage.h"
 #include "ui_resultpage.h"
@@ -36,7 +37,6 @@
 #include "utils/validation.h"
 #include "utils/filedialog.h"
 #include "utils/keyparameters.h"
-#include "utils/scrollarea.h"
 #include "utils/userinfo.h"
 
 #include <Libkleo/Compat>
@@ -63,7 +63,6 @@
 #include <KMessageBox>
 #include <QIcon>
 
-#include <QCommandLinkButton>
 #include <QRegularExpressionValidator>
 #include <QLineEdit>
 #include <QMetaProperty>
@@ -183,15 +182,6 @@ static bool is_eddsa(unsigned int algo)
 static bool is_ecdh(unsigned int algo)
 {
     return is_algo(static_cast<Subkey::PubkeyAlgo>(algo), ECDH);
-}
-
-static void force_set_checked(QAbstractButton *b, bool on)
-{
-    // work around Qt bug (tested: 4.1.4, 4.2.3, 4.3.4)
-    const bool autoExclusive = b->autoExclusive();
-    b->setAutoExclusive(false);
-    b->setChecked(b->isEnabled() && on);
-    b->setAutoExclusive(autoExclusive);
 }
 
 static void set_keysize(QComboBox *cb, unsigned int strength)
@@ -721,101 +711,6 @@ private:
     Ui_AdvancedSettingsDialog ui;
     bool mECCSupported;
     bool mEdDSASupported;
-};
-
-class ChooseProtocolPage : public WizardPage
-{
-    Q_OBJECT
-
-    struct {
-        QCommandLinkButton *pgpCLB;
-        QCommandLinkButton *x509CLB;
-
-        void setupUi(QWizardPage *parent)
-        {
-            parent->setTitle(i18nc("@title", "Choose Type of Key Pair"));
-            parent->setSubTitle(i18n("Please choose which type of key pair you want to create."));
-
-            auto mainLayout = new QVBoxLayout{parent};
-            mainLayout->setContentsMargins(0, 0, 0, 0);
-
-            auto scrollArea = new ScrollArea{parent};
-            scrollArea->setFocusPolicy(Qt::NoFocus);
-            scrollArea->setFrameStyle(QFrame::NoFrame);
-            scrollArea->setBackgroundRole(parent->backgroundRole());
-            scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-            auto scrollAreaLayout = qobject_cast<QBoxLayout *>(scrollArea->widget()->layout());
-            scrollAreaLayout->setContentsMargins(0, 0, 0, 0);
-
-            pgpCLB = new QCommandLinkButton{parent};
-            pgpCLB->setText(i18n("Create a personal OpenPGP key pair"));
-            pgpCLB->setDescription(i18n("OpenPGP key pairs are certified by confirming the fingerprint of the public key."));
-            pgpCLB->setAccessibleDescription(pgpCLB->description());
-            pgpCLB->setCheckable(true);
-            pgpCLB->setAutoExclusive(true);
-
-            scrollAreaLayout->addWidget(pgpCLB);
-
-            x509CLB = new QCommandLinkButton{parent};
-            x509CLB->setText(i18n("Create a personal X.509 key pair and certification request"));
-            x509CLB->setDescription(i18n("X.509 key pairs are certified by a certification authority (CA). The generated request needs to be sent to a CA to finalize creation."));
-            x509CLB->setAccessibleDescription(x509CLB->description());
-            x509CLB->setCheckable(true);
-            x509CLB->setAutoExclusive(true);
-
-            scrollAreaLayout->addWidget(x509CLB);
-
-            scrollAreaLayout->addStretch(1);
-
-            mainLayout->addWidget(scrollArea);
-        }
-    } ui;
-
-public:
-    explicit ChooseProtocolPage(QWidget *p = nullptr)
-        : WizardPage{p}
-        , ui{}
-    {
-        setObjectName(QStringLiteral("Kleo__NewCertificateUi__ChooseProtocolPage"));
-        ui.setupUi(this);
-        registerField(QStringLiteral("pgp"), ui.pgpCLB);
-    }
-
-    void setProtocol(Protocol proto)
-    {
-        if (proto == OpenPGP) {
-            ui.pgpCLB->setChecked(true);
-        } else if (proto == CMS) {
-            ui.x509CLB->setChecked(true);
-        } else {
-            force_set_checked(ui.pgpCLB,  false);
-            force_set_checked(ui.x509CLB, false);
-        }
-    }
-
-    Protocol protocol() const
-    {
-        return
-            ui.pgpCLB->isChecked()  ? OpenPGP :
-            ui.x509CLB->isChecked() ? CMS : UnknownProtocol;
-    }
-
-    void initializePage() override {
-        if (!initialized)
-        {
-            connect(ui.pgpCLB,  &QAbstractButton::clicked, wizard(), &QWizard::next, Qt::QueuedConnection);
-            connect(ui.x509CLB, &QAbstractButton::clicked, wizard(), &QWizard::next, Qt::QueuedConnection);
-        }
-        initialized = true;
-    }
-
-    bool isComplete() const override
-    {
-        return protocol() != UnknownProtocol;
-    }
-
-private:
-    bool initialized = false;
 };
 
 struct Line {

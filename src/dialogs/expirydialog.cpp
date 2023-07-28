2d42d28c2a4d541d7cdc9e7fe7f15bc45b7b8df5
Ingo Klöcker <dev@ingo-kloecker.de>
2023-07-28 11:07:37 +0200

    Unify selection of expiration date in different dialogs
    Remove the additional possibility to set the expiration date by a certain
    amount of days/weeks/months/years from today that was only present in the
    Change Validitiy Period dialog.
    
    GnuPG-bug-id: 6621

--- src/dialogs/expirydialog.cpp
+++ src/dialogs/expirydialog.cpp
@@ -25,131 +25,46 @@
 #include <KStandardGuiItem>
 
 #include <QCheckBox>
-#include <QComboBox>
 #include <QDate>
 #include <QDialogButtonBox>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QPushButton>
 #include <QRadioButton>
-#include <QSpinBox>
 #include <QVBoxLayout>
 
 using namespace Kleo;
 using namespace Kleo::Dialogs;
-
-namespace
-{
-
-enum Period {
-    Days,
-    Weeks,
-    Months,
-    Years,
-
-    NumPeriods
-};
-
-static QDate date_by_amount_and_unit(int inAmount, int inUnit)
-{
-    const QDate current = QDate::currentDate();
-    switch (inUnit) {
-    case Days:   return current.addDays(inAmount);
-    case Weeks:  return current.addDays(7 * inAmount);
-    case Months: return current.addMonths(inAmount);
-    case Years:  return current.addYears(inAmount);
-    default:
-        Q_ASSERT(!"Should not reach here");
-    }
-    return QDate();
-}
-
-static QString accessibleValidityDuration(int amount, Period unit)
-{
-    switch (unit) {
-    case Days:
-        return i18np("Valid for %1 day", "Valid for %1 days", amount);
-    case Weeks:
-        return i18np("Valid for %1 week", "Valid for %1 weeks", amount);
-    case Months:
-        return i18np("Valid for %1 month", "Valid for %1 months", amount);
-    case Years:
-        return i18np("Valid for %1 year", "Valid for %1 years", amount);
-    default:
-        Q_ASSERT(!"invalid unit");
-    }
-    return {};
-}
-
-// these calculations should be precise enough for the forseeable future...
-static const double DAYS_IN_GREGORIAN_YEAR = 365.2425;
-
-static int monthsBetween(const QDate &d1, const QDate &d2)
-{
-    const int days = d1.daysTo(d2);
-    return qRound(days / DAYS_IN_GREGORIAN_YEAR * 12);
-}
-
-static int yearsBetween(const QDate &d1, const QDate &d2)
-{
-    const int days = d1.daysTo(d2);
-    return qRound(days / DAYS_IN_GREGORIAN_YEAR);
-}
-
-}
 
 class ExpiryDialog::Private
 {
     friend class ::Kleo::Dialogs::ExpiryDialog;
     ExpiryDialog *const q;
 public:
-    explicit Private(Mode mode, ExpiryDialog *qq)
+    Private(Mode mode, ExpiryDialog *qq)
         : q{qq}
-        , mode{mode}
-        , inUnit{Days}
-        , ui{mode, q}
+        , ui{mode, qq}
     {
         ui.neverRB->setEnabled(unlimitedValidityAllowed());
-        ui.inRB->setEnabled(!fixedExpirationDate());
         ui.onRB->setEnabled(!fixedExpirationDate());
 
-#if QT_DEPRECATED_SINCE(5, 14)
-        connect(ui.inSB, qOverload<int>(&QSpinBox::valueChanged),
-                q, [this] () { slotInAmountChanged(); });
-#else
-        connect(ui.inSB, &QSpinBox::valueChanged,
-                q, [this] () { slotInAmountChanged(); });
-#endif
-        connect(ui.inCB, qOverload<int>(&QComboBox::currentIndexChanged),
-                q, [this] () { slotInUnitChanged(); });
         connect(ui.onCB, &KDateComboBox::dateChanged,
                 q, [this] () { slotOnDateChanged(); });
-
-        Q_ASSERT(ui.inCB->currentIndex() == inUnit);
     }
 
 private:
-    void slotInAmountChanged();
-    void slotInUnitChanged();
     void slotOnDateChanged();
 
 private:
     bool unlimitedValidityAllowed() const;
     bool fixedExpirationDate() const;
-    QDate inDate() const;
-    int inAmountByDate(const QDate &date) const;
     void setInitialFocus();
 
 private:
-    ExpiryDialog::Mode mode;
-    int inUnit;
     bool initialFocusWasSet = false;
 
     struct UI {
         QRadioButton *neverRB;
-        QRadioButton *inRB;
-        QSpinBox *inSB;
-        QComboBox *inCB;
         QRadioButton *onRB;
         KDateComboBox *onCB;
         QCheckBox *updateSubkeysCheckBox;
@@ -175,36 +90,6 @@ private:
             neverRB->setChecked(false);
 
             vboxLayout->addWidget(neverRB);
-
-            {
-                auto hboxLayout = new QHBoxLayout;
-
-                inRB = new QRadioButton{i18n("Valid for:"), mainWidget};
-                inRB->setChecked(false);
-
-                hboxLayout->addWidget(inRB);
-
-                inSB = new QSpinBox{mainWidget};
-                inSB->setEnabled(false);
-                inSB->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
-                inSB->setMinimum(1);
-
-                hboxLayout->addWidget(inSB);
-
-                inCB = new QComboBox{mainWidget};
-                inCB->addItem(i18n("Days"));
-                inCB->addItem(i18n("Weeks"));
-                inCB->addItem(i18n("Months"));
-                inCB->addItem(i18n("Years"));
-                Q_ASSERT(inCB->count() == NumPeriods);
-                inCB->setEnabled(false);
-
-                hboxLayout->addWidget(inCB);
-
-                hboxLayout->addStretch(1);
-
-                vboxLayout->addLayout(hboxLayout);
-            }
 
             {
                 auto hboxLayout = new QHBoxLayout;
@@ -250,37 +135,12 @@ private:
             mainLayout->addWidget(buttonBox);
 
             connect(onRB, &QRadioButton::toggled, onCB, &QWidget::setEnabled);
-            connect(inRB, &QRadioButton::toggled, inCB, &QWidget::setEnabled);
-            connect(inRB, &QRadioButton::toggled, inSB, &QWidget::setEnabled);
         }
     } ui;
 };
 
-void ExpiryDialog::Private::slotInUnitChanged()
-{
-    const int oldInAmount = ui.inSB->value();
-    const QDate targetDate = date_by_amount_and_unit(oldInAmount, inUnit);
-    inUnit = ui.inCB->currentIndex();
-    if (targetDate.isValid()) {
-        ui.inSB->setValue(inAmountByDate(targetDate));
-    } else {
-        slotInAmountChanged();
-    }
-}
-
-void ExpiryDialog::Private::slotInAmountChanged()
-{
-    if (ui.inRB->isChecked()) {
-        ui.onCB->setDate(inDate());
-    }
-    ui.inRB->setAccessibleName(accessibleValidityDuration(ui.inSB->value(), static_cast<Period>(ui.inCB->currentIndex())));
-}
-
 void ExpiryDialog::Private::slotOnDateChanged()
 {
-    if (!ui.inRB->isChecked()) {
-        ui.inSB->setValue(inAmountByDate(ui.onCB->date()));
-    }
     ui.onRB->setAccessibleName(i18nc("Valid until DATE", "Valid until %1", Formatting::accessibleDate(ui.onCB->date())));
 }
 
@@ -294,32 +154,13 @@ bool Kleo::Dialogs::ExpiryDialog::Private::fixedExpirationDate() const
     return ui.onCB->minimumDate() == ui.onCB->maximumDate();
 }
 
-QDate ExpiryDialog::Private::inDate() const
-{
-    return date_by_amount_and_unit(ui.inSB->value(), ui.inCB->currentIndex());
-}
-
-int ExpiryDialog::Private::inAmountByDate(const QDate &selected) const
-{
-    const QDate current  = QDate::currentDate();
-
-    switch (ui.inCB->currentIndex()) {
-    case Days:   return current.daysTo(selected);
-    case Weeks:  return qRound(current.daysTo(selected) / 7.0);
-    case Months: return monthsBetween(current, selected);
-    case Years:  return yearsBetween(current, selected);
-    };
-    Q_ASSERT(!"Should not reach here");
-    return -1;
-}
-
 void ExpiryDialog::Private::setInitialFocus()
 {
     if (initialFocusWasSet) {
         return;
     }
     // give focus to the checked radio button
-    (void) focusFirstCheckedButton({ui.neverRB, ui.inRB, ui.onRB});
+    (void) focusFirstCheckedButton({ui.neverRB, ui.onRB});
     initialFocusWasSet = true;
 }
 
@@ -354,10 +195,7 @@ void ExpiryDialog::setDateOfExpiry(const QDate &date)
 
 QDate ExpiryDialog::dateOfExpiry() const
 {
-    return
-        d->ui.inRB->isChecked() ? d->inDate() :
-        d->ui.onRB->isChecked() ? d->ui.onCB->date() :
-        QDate{};
+    return d->ui.onRB->isChecked() ? d->ui.onCB->date() : QDate{};
 }
 
 void ExpiryDialog::setUpdateExpirationOfAllSubkeys(bool update)

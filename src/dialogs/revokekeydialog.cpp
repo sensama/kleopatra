9beaf667db163ab9f4fdf5e341875f70d7794d9d
Tobias Fella <tobias.fella@gnupg.com>
2024-04-10 10:02:28 +0200

    Indent certificate

--- src/dialogs/revokekeydialog.cpp
+++ src/dialogs/revokekeydialog.cpp
@@ -281,7 +281,8 @@ void RevokeKeyDialog::setKey(const GpgME::Key &key)
     d->key = key;
     d->ui.infoLabel->setText(
         xi18nc("@info",
-               "<para>You are about to revoke the following certificate:</para><para>%1</para><para><emphasis strong='true'>The revocation will take effect "
+               "<para>You are about to revoke the following certificate:</para><para>&nbsp;&nbsp;&nbsp;&nbsp;%1</para><para><emphasis strong='true'>The "
+               "revocation will take effect "
                "immediately and "
                "cannot be reverted.</emphasis></para><para>It will no longer be possible to sign using this certificate.<nl/>It will still be possible to "
                "decrypt using this certificate.<nl/>Other people will no longer be able to encrypt for this certificate after receiving the revocation.</para>")

fbc6ed9f8ffe84e2beac00d43e9678d4ea43215d
Tobias Fella <tobias.fella@gnupg.com>
2024-07-09 15:31:14 +0200

    Fix initial column visibility in main key tree
    GnuPG-Bug-ID: 6957

--- src/view/tabwidget.cpp
+++ src/view/tabwidget.cpp
@@ -1013,6 +1013,12 @@ QAbstractItemView *TabWidget::addView(const QString &title, const QString &id, c
     Page *page = new Page(title, id, text, nullptr, QString(), nullptr, group, d->keyTreeViewOptions);
     group.writeEntry(KEY_FILTER_ENTRY, id);
     group.sync();
+    QMetaObject::invokeMethod(
+        this,
+        [page, group]() {
+            page->restoreLayout(group);
+        },
+        Qt::QueuedConnection);
     return d->addView(page, d->currentPage());
 }
 
